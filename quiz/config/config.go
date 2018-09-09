279fa73a0bc515a9a275a9038d47f258bd4ef999
Apipol Sukgler <apipol@computerlogy.com>
2018-09-09 14:44:52 +0700

    [Edited]: remove unused code

--- quiz/config/config.go
+++ quiz/config/config.go
@@ -17,7 +17,6 @@ func GetConfiguration(environment string) map[string]string {
 
 func GetConfigurationFile(environment string) map[string]string {
 	directory, _ := os.Getwd()
-	fmt.Printf("%s/config/%s.json", directory, environment)
 	configBytes, _ := ioutil.ReadFile(fmt.Sprintf("%s/config/%s.json", directory, environment))
 	configJSON := map[string]string{}
 	json.Unmarshal(configBytes, &configJSON)
